d68198acf1a4652a480b991d8c9acb2bb2cd0b45
ny-tendry <nytendry2@gamil.com>
2026-09-06 01:41:55 +0300

    update url prod-2

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:8080"
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://stuntix.nytendry2.workers.dev"
+        "https://stuntix.nytendry2.workers.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
