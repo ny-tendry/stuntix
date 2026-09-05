bc7409bdc2ba0db8f5d29ce7fcd5d8f94fd6d053
ny-tendry <nytendry2@gamil.com>
2026-09-06 00:38:01 +0300

    dockerize frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080"
         "http://localhost:5500",
         "http://127.0.0.1:5500",
     ],
